631f963da08627864ccd08000826c1cf1c10f169
GideonJagen <gideon.jagenstedt@gmail.com>
2024-03-17 20:27:49 +0100

    Add working detection and tracking using loadstar and magik

---
+++ track.py
@@ -0,0 +1,137 @@
+import sys
+from pathlib import Path
+import os
+
+module_path = os.path.abspath(os.path.join(".."))
+sys.path.append(Path(module_path))
+
+import deeptrack as dt
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+from matplotlib import cm
+import itertools
+
+
+def predict_trajectories(model, detections, radius=0.08, nframes=7, threshold=6):
+    """
+    Predict trajectories using the given model and detections.
+
+    Args:
+        model: The trajectory prediction model.
+        detections (pandas.DataFrame): Detected objects.
+        radius (float): Radius parameter for trajectory prediction.
+        nframes (int): Number of frames parameter for trajectory prediction.
+        threshold (int): Trajectory length threshold parameter for trajectory prediction.
+
+    Returns:
+        pandas.DataFrame: Predicted trajectories.
+    """
+    variables = dt.DummyFeature(
+        radius=radius,
+        output_type="edges",
+        nofframes=nframes,
+    )
+
+    print("Predicting trajectories...")
+    pred, gt, scores, graph = dt.models.gnns.get_predictions(
+        detections, ["centroid"], model, **variables.properties()
+    )
+    print("Creating dataframe...")
+    edges_df, nodes, _ = dt.models.gnns.df_from_results(pred, gt, scores, graph)
+
+    print("Getting trajectories...")
+    trajs = dt.models.gnns.to_trajectories(edges_df=edges_df)
+    trajs = list(filter(lambda t: len(t) > threshold, trajs))
+    trajs = [sorted(t) for t in trajs]
+
+    object_index = np.zeros((nodes.shape[0]))
+    for i, traj in enumerate(trajs):
+        object_index[traj] = i + 1
+
+    nodes_df = pd.DataFrame(
+        {
+            "frame": nodes[:, 0],
+            "x": nodes[:, 1],
+            "y": nodes[:, 2],
+            "entity": object_index,
+        }
+    )
+    nodes_df = nodes_df[nodes_df["entity"] != 0]
+    print("Done!")
+
+    return nodes_df
+
+
+def save_trajectories(traj_df, path):
+    """
+    Save the predicted trajectories to a CSV file.
+
+    Args:
+        traj_df (pandas.DataFrame): Predicted trajectories.
+        path (str): Path to save the CSV file.
+    """
+    traj_df.to_csv(path, index=False)
+
+
+def _f(d):
+    max_scores = d.groupby("frame_y")["score"].transform("max")
+    d.loc[d["score"] < max_scores, "prediction"] = False
+    return d
+
+
+def _to_trajectories(
+    edges_df: pd.DataFrame,
+    assert_no_splits=True,
+    _type="prediction",
+):
+    edges_df["frame_diff"] = edges_df["frame_y"] - edges_df["frame_x"]
+    if assert_no_splits and _type != "gt":
+        edges_df_grouped = edges_df.groupby(["node_x"])
+        edges_df = edges_df_grouped.apply(_f)
+    edges_df["frame_diff"] = edges_df["frame_y"] - edges_df["frame_x"]
+    edges_df_grouped = edges_df.groupby(["node_y"])
+    edges_dfs = [_edge_df for _, _edge_df in edges_df_grouped]
+
+    color = cm.viridis(np.linspace(0, 1, 250))
+    color = itertools.cycle(color)
+    t = defaultdict(lambda: next(color))
+
+    x_iterator = iter(range(10000000))
+    tr = {}
+    sc = {}
+    alpha = 0.1
+    parents = set()
+    d_alpha = (1 - alpha) / edges_df["prediction"].max()
+    trajectories = []
+    for _edge_df in edges_dfs:
+        solutions = _edge_df.loc[_edge_df[_type] == 1.0, :]
+        solutions = solutions.loc[
+            solutions["frame_diff"] == solutions["frame_diff"].min(), :
+        ]
+        if _type == "prediction":
+            solutions = solutions.loc[solutions["score"] == solutions["score"].max(), :]
+        for i in range(len(solutions)):
+            node_x = int(solutions[i : i + 1]["node_x"])
+            alpha = 0.1 + d_alpha * int(solutions[i : i + 1]["frame_y"])
+
+            if node_x not in t:
+                tr[node_x] = next(x_iterator)
+                sc[node_x] = float(solutions[i : i + 1]["score"])
+
+            if node_x in parents:
+                break
+
+            t[str(int(solutions[i : i + 1]["node_y"]))] = t[str(node_x)]
+            tr[int(solutions[i : i + 1]["node_y"])] = tr[node_x]
+            parents.add(node_x)
+
+    traj_dict = defaultdict(list)
+
+    for key, val in tr.items():
+        traj_dict[val].append(key)
+
+    for val in traj_dict.values():
+        trajectories.append(val)
+
+    return trajectories
---
+++ utils.py
@@ -0,0 +1,230 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import deeptrack as dt
+
+from matplotlib import pyplot as plt
+from matplotlib import cm
+
+from tqdm import tqdm
+import gc
+import tifffile
+from pathlib import Path
+
+import itertools
+from collections import defaultdict
+import warnings
+import io
+import sys
+
+
+def load_models(
+    loadstar_weight_name="Loadstar_EV_24462", magik_weight_name="MAGIK_MP_MPN"
+):
+    """
+    Load the Loadstar and MAGIK models.
+
+    Args:
+        loadstar_weight_name (str): Name of the Loadstar weight file.
+        magik_weight_name (str): Name of the MAGIK weight file.
+
+    Returns:
+        tuple: Tuple containing the loaded Loadstar and MAGIK models.
+    """
+    loadstar = load_loadstar_model(loadstar_weight_name)
+    magik = load_magik_model(magik_weight_name)
+
+    return loadstar, magik
+
+
+def load_loadstar_model(weight_name="Loadstar_EV_24462"):
+    """
+    Load the Loadstar model.
+
+    Args:
+        weight_name (str): Name of the Loadstar weight file.
+
+    Returns:
+        deeptrack.models.LodeSTAR: Loaded Loadstar model.
+    """
+    loadstar = dt.models.LodeSTAR(input_shape=(None, None, 1))
+    loadstar.load_weights(Path(f"../weights/loadstar/{weight_name}/{weight_name}"))
+    return loadstar
+
+
+def load_magik_model(weight_name="MAGIK_MP_MPN"):
+    """
+    Load the MAGIK model.
+
+    Args:
+        weight_name (str): Name of the MAGIK weight file.
+
+    Returns:
+        deeptrack.models.gnns.MAGIK: Loaded MAGIK model.
+    """
+    magik = dt.models.gnns.MAGIK(
+        dense_layer_dimensions=(
+            64,
+            96,
+        ),
+        base_layer_dimensions=(
+            96,
+            96,
+            96,
+        ),
+        number_of_node_features=2,
+        number_of_edge_features=1,
+        number_of_edge_outputs=1,
+        edge_output_activation="sigmoid",
+        output_type="edges",
+        graph_block="MPN",
+    )
+
+    # Need to supress warnings for loading the model as MAGIK is not compatible expect_partial()
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        magik.load_weights(Path(f"../weights/magik/{weight_name}.h5"))
+
+    return magik
+
+
+def detect(tiff_path, loadstar, batch_size=10, alpha=0.999, cutoff=1e-2, plot=False):
+    """
+    Detect objects in a TIFF file using the Loadstar model.
+
+    Args:
+        tiff_path (str): Path to the TIFF file.
+        loadstar (deeptrack.models.LodeSTAR): Loaded Loadstar model.
+        batch_size (int): Number of frames to process in each batch.
+        alpha (float): Alpha parameter for detection.
+        cutoff (float): Cutoff parameter for detection.
+        plot (bool): Whether to plot the detected objects.
+
+    Returns:
+        pandas.DataFrame: Detected objects.
+    """
+    detections_list = []
+
+    file = tifffile.TiffFile(Path(tiff_path))
+
+    n_pages = len(file.pages)
+    iterations = n_pages // batch_size
+
+    with tqdm(total=iterations, position=0, leave=True) as pbar:
+        for i in tqdm(range(iterations), position=0, leave=True):
+            pbar.update()
+            pages = file.pages[i * batch_size : min((i + 1) * batch_size, n_pages - 1)]
+            frames = np.array([page.asarray() for page in pages])
+            frames = frames.astype(np.float32)
+            frames = frames - frames.mean()
+            frames = frames / np.std(frames, axis=(0, 1, 2), keepdims=True) / 3
+
+            frames = np.expand_dims(frames, -1)
+
+            stdout_trap = io.StringIO()
+            sys.stdout = stdout_trap
+            detections_all = loadstar.predict_and_detect(
+                frames, alpha=alpha, beta=1 - alpha, cutoff=cutoff, mode="ratio"
+            )
+            sys.stdout = sys.__stdout__
+
+            detections_list.append(detections_all)
+
+            if plot:
+                for j, detections in enumerate(detections_all):
+                    plt.figure(figsize=(10, 10))
+                    plt.imshow(frames[j], cmap="gray")
+                    plt.scatter(
+                        detections[:, 1],
+                        detections[:, 0],
+                        marker="o",
+                        color="r",
+                        s=100,
+                        facecolors="none",
+                    )
+                    plt.savefig(
+                        "../output/detected_images/fig_{}.png".format(
+                            str(i * batch_size + j).zfill(4)
+                        )
+                    )
+
+                    plt.cla()
+                    plt.clf()
+                    plt.close("all")
+                    gc.collect()
+
+    detections_df = _detections_to_df(detections_list, file.pages[0].shape)
+
+    return detections_df
+
+
+def save_detections(detection_df, path, full=False):
+    """
+    Save the detected objects to a CSV file.
+
+    Args:
+        detection_df (pandas.DataFrame): Detected objects.
+        path (str): Path to save the CSV file.
+        full (bool, optional): If True, save the full dataframe including all columns.
+                               If False, save a modified dataframe without the "set", "label", and "solution" columns.
+                               Defaults to False.
+    """
+    detection_df_d = detection_df.copy()
+    if not full:
+        detection_df_d.drop(columns=["set", "label", "solution"], inplace=True)
+    detection_df_d.to_csv(path, index=False)
+
+
+def load_detections(path):
+    """
+    Save the detected objects to a CSV file.
+
+    Args:
+        detection_df (pandas.DataFrame): Detected objects.
+        path (str): Path to save the CSV file.
+    """
+    # detection_df_d.drop(columns=["set", "label", "solution"], inplace=True)
+    # detection_df_d.to_csv(path, index=False)
+    detection_df = pd.read_csv(path)
+    return detection_df
+
+
+def _detections_to_df(detections_list, file_page_shape):
+    size = 0
+    for detections_all in detections_list:
+        for detections in detections_all:
+            size += detections.shape[0]
+    print(size)
+
+    detection_array = np.zeros((size, 6))
+
+    traversed = 0
+    list_traversed = 0
+    for i, detections_all in enumerate(detections_list):
+        for j, detections in enumerate(detections_all):
+            detection_array[traversed : traversed + detections.shape[0], 0] = (
+                list_traversed + j
+            )
+            detection_array[traversed : traversed + detections.shape[0], 4:] = (
+                detections
+            )
+            traversed += detections.shape[0]
+        list_traversed += len(detections_all)
+
+    detection_df = pd.DataFrame(
+        detection_array,
+        columns=["frame", "set", "label", "solution", "centroid-1", "centroid-0"],
+    ).astype(
+        {
+            "set": "int32",
+            "frame": "int32",
+            "label": "int32",
+            "solution": "int32",
+            "centroid-1": "float32",
+            "centroid-0": "float32",
+        }
+    )
+    detection_df["centroid-1"] /= file_page_shape[0]
+    detection_df["centroid-0"] /= file_page_shape[1] / 10
+
+    return detection_df
